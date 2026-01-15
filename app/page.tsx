8a18bcbe92f713b6488620308b177439df3fb85d
karutt <karutetto332@gmail.com>
2026-01-15 20:49:24 +0900

    feat: update page component to render links and add logo
    - Removed redirect functionality and replaced it with a return statement that renders the Signup and Login links.
    - Added an Image component to display the logo.
    - Introduced a new logo image file in the public directory.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
-import { redirect } from 'next/navigation';
-
+import Link from 'next/link';
+import Image from 'next/image';
 export default function Page() {
-    redirect('/signup');
+    return (
+        <div className="w-full h-dvh flex justify-center items-center flex-col gap-4">
+            <Link href="/signup" className="text-blue-600 underline">
+                Go to Signup Page
+            </Link>
+            <Link href="/signin" className="text-blue-600 underline">
+                Go to Login Page
+            </Link>
+            <Image src="/png/logo.png" alt="Logo" width={24} height={24} />
+        </div>
+    );
 }
